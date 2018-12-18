e8b140d6fa7eac093e5c9623916be36ee1c2f55e
alfonslange <alfons_lange97@hotmail.com>
2018-12-18 16:27:15 +0100

    small fix

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worlders!" );
+        System.out.println( "Hello Worlders!" );
     }
 }
